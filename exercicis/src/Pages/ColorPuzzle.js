6ca6c9d3cef50945c29544ad71d1611380d431ba
amanamar24 <amanamar24@gmail.com>
2022-07-13 22:22:40 +0200

    sortBallColor2

--- exercicis/src/Pages/ColorPuzzle.js
+++ exercicis/src/Pages/ColorPuzzle.js
@@ -6,6 +6,7 @@ export default function ColorPuzzle() {
   // i still got it
   const [clickeado, setClickeado] = useState(null);
   const [jugadas, setJugadas] = useState([]);
+  const [colorTranfer, setColorTransfer] = useState(null);
   let initialState = [
     // añadir show : true, false
     [0, 2, 4, 3],
@@ -25,40 +26,46 @@ export default function ColorPuzzle() {
     // console.log(jugadas.length);
     let resultKeys = null;
     let auxjugada = {};
-    if (jugadas.length === 0) {
+    let j = 0;
+    resultKeys = Object.keys(jugadas[jugadas.length - 1]).length;
+    while (j < distribution[i].length - 1 && distribution[i][j] === 5) {
+      // el 5 representa white
+      j++;
+    }
+    let ballFounded = j--;
+
+    if (jugadas.length === 0 || resultKeys !== 2) {
+      //¬(A||B) = ¬A && ¬B
+      //jugadas.length ===0 is for the first entry
+      //or resultKeys !== 2
       // or the previous was full
       // register the first historic
 
-      auxjugada = { firstEpr: i, firstBall: 0 };
+      auxjugada = { firstEpr: i, firstBall: ballFounded };
+      setColorTransfer(distribution[i][ballFounded]);
     } else {
       // diferenciate between 2 components or 4
       //const result = Object.keys(student).length;
       // diferenciate empty by colors
-      resultKeys = Object.keys(jugadas[jugadas.length - 1]).length;
-      if (resultKeys === 2) {
-        // to diferenciate the first click
-        jugadas.pop();
-        // update the next sequences based in the previous object
-        auxjugada = {
-          // firstEpr: clickeado,
-          // taking data from the previous click registred in jugadas
-          firstEpr: jugadas[jugadas.length - 1].firstEpr,
-          firstBall: jugadas[jugadas.length - 1].firstBall,
-          // taking data from the previous click registred in jugadas
-          secondEpr: i,
-          secondBall: 3,
-        };
-      } else {
-        auxjugada = { firstEpr: i, firstBall: 0 };
-      }
+
+      // searching the last white ball in distribution
+      // to diferenciate the first click
+
+      // update the next sequences based in the previous object
+      auxjugada = {
+        // firstEpr: clickeado,
+        // taking data from the previous click registred in jugadas
+        firstEpr: jugadas[jugadas.length - 1].firstEpr,
+        firstBall: jugadas[jugadas.length - 1].firstBall,
+        // taking data from the previous click registred in jugadas
+        secondEpr: i,
+        secondBall: ballFounded,
+        // in i eprouvette searching the last white ball
+        // in distribution
+      };
+      jugadas.pop();
 
       // setJugadas([...jugadas]);
-      auxjugada = {
-        firstEpr: clickeado,
-        firstBall: 0,
-        secondEpr: i,
-        secondBall: 3,
-      };
     }
     jugadas.push(auxjugada);
     setJugadas([...jugadas]);
@@ -66,6 +73,10 @@ export default function ColorPuzzle() {
     console.log(jugadas);
     let aux = [...distribution];
     // we start here the sort algorithm
+    // 2 options working with jugadas(i could have a delay problem) or working
+    // with auxjugada
+    // the change is in first click making the ball white
+    // in the second click transfering the color of the first ball before making it white
     aux[i][0] = 5;
     // eprouvette i position 0 jugadas:[{epr1:clickeado,ball1:0,epr2:i,ball2:}]
     // if jugadas empty
